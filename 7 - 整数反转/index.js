500d60afd2eccfc5e809ab210ef894490469bef2
chenfei <chenfei_yq@kedacom.com>
2022-06-02 16:07:09 +0800

    整数反转

---
+++ 7 - 整数反转/index.js
@@ -0,0 +1,15 @@
+var reverse = function (x) {
+  let rev = 0;
+  while (x !== 0) {
+    const digit = x % 10;
+    x = ~~(x / 10);
+		rev = rev * 10 + digit
+		console.log(digit, x, rev)
+		if (rev < Math.pow(-2, 31) || rev > Math.pow(2, 31) - 1) {
+			return 0
+		}
+  }
+	return rev
+};
+
+reverse(-123)
